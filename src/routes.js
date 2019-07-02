9ed364c8984bcab4e27abd67ce3083a37e162afe
alpaca623 <alpaca623@gmail.com>
2019-07-02 23:30:48 +0900

    [adding] : add route source

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,20 @@ const LOGOUT = "/logout";
 
 const TODAY_AUTH = "/authentication";
 
+const AUTH = "/auth";
+const AUTH_REQUEST = "/request";
+// const AUTH_REQUEST_LIST = "/list";
+
+const KAKAO_AUTH = "/kakao";
+const KAKAO_CALLBACK = "/login/kakao/callback";
+
 export const routes = {
-  home:HOME,
-  login:LOGIN,
-  logout:LOGOUT,
-  today_auth:TODAY_AUTH
+  home: HOME,
+  login: LOGIN,
+  logout: LOGOUT,
+  today_auth: TODAY_AUTH,
+  auth: AUTH,
+  auth_request: AUTH_REQUEST,
+  kakaoAuth: KAKAO_AUTH,
+  kakaoCallback: KAKAO_CALLBACK
 };
