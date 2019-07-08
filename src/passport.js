1885839857b7be460a83b9ed8ee8bd9978ee865e
alpaca623 <alpaca623@gmail.com>
2019-07-08 23:32:26 +0900

    [update] : delete source kakao strategy for passport

--- src/passport.js
+++ src/passport.js
@@ -1,19 +1,5 @@
 import passport from "passport";
-import kakaoStrategy from "passport-kakao";
 import { routes } from "./routes";
 import User from "./models/User";
 
 passport.use(User.createStrategy());
-
-passport.use(
-  new kakaoStrategy(
-    {
-      clientID: process.env.KAKAO_SECRET,
-      // clientSecret: process.env.KAKAO_SECRET,
-      callbackURL: routes.kakaoCallback
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile, done);
-    }
-  )
-);
